8cf783165d8968a913903aa0d886a28761288d0d
Dating Libre <admin@datinglibre.com>
2021-04-10 14:47:39 +0100

    Convert xml configuration to yaml

--- src/DependencyInjection/DatingLibreAppExtension.php
+++ src/DependencyInjection/DatingLibreAppExtension.php
@@ -7,15 +7,15 @@ namespace DatingLibre\AppBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class DatingLibreAppExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
-        $loader->load('services.xml');
+        $loader->load('services.yaml');
 
         $configuration = $this->processConfiguration(new Configuration(), $configs);
         $container->setParameter('datinglibre.categories', $configuration['categories']);
